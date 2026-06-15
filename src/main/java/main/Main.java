afab4e32d21eafb8a43f2fd848a26359cef04066
juliansummer <wi25b085@technikum-wien.at>
2026-06-15 14:51:00 +0200

    add base TicTacToe game with Board, Player and TicTacToe classes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,11 @@
-package Main;
+package main;
+
+import tictactoe.TicTacToe;
 
 public class Main {
     static void main() {
-        
-    }
+            TicTacToe game = new TicTacToe();
+            game.start();
+        }
+
 }
