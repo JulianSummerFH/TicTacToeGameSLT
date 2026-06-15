4dd8e8d595d1dfca37aae9ef170de21178fed807
juliansummer <wi25b085@technikum-wien.at>
2026-06-15 15:00:14 +0200

    refactor: split start() method to comply with checkstyle

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -3,7 +3,6 @@ package tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-
     private Player player1;
     private Player player2;
     private Player currentPlayer;
@@ -19,45 +18,50 @@ public class TicTacToe {
     public void start() {
         Scanner scanner = new Scanner(System.in);
         boolean playing = true;
-
         while (playing) {
-            board.print();
-            System.out.println("Current Player: " + currentPlayer.getMarker());
-            System.out.print("row (0-2): ");
-            int row = scanner.nextInt();
-            System.out.print("column (0-2): ");
-            int col = scanner.nextInt();
-
-            if (!board.isCellEmpty(row, col)) {
-                System.out.println("Cell is already taken, try again!");
-                continue;
-            }
-
-            board.place(row, col, currentPlayer.getMarker());
-
-            if (hasWinner()) {
-                board.print();
-                System.out.println("Player " + currentPlayer.getMarker() + " wins!");
-                playing = false;
-            } else if (board.isFull()) {
-                board.print();
-                System.out.println("It's a draw!");
-                playing = false;
-            } else {
-                switchCurrentPlayer();
-            }
-
-            if (!playing) {
-                System.out.print("Play again? (y/n): ");
-                String answer = scanner.next();
-                if (answer.equals("y")) {
-                    board.clear();
-                    currentPlayer = player1;
-                    playing = true;
-                }
-            }
+            playing = playRound(scanner);
         }
         scanner.close();
+    }
+
+    private boolean playRound(Scanner scanner) {
+        board.print();
+        System.out.println("Current Player: " + currentPlayer.getMarker());
+        System.out.print("row (0-2): ");
+        int row = scanner.nextInt();
+        System.out.print("column (0-2): ");
+        int col = scanner.nextInt();
+        if (!board.isCellEmpty(row, col)) {
+            System.out.println("Cell is already taken, try again!");
+            return true;
+        }
+        board.place(row, col, currentPlayer.getMarker());
+        return checkGameState(scanner);
+    }
+
+    private boolean checkGameState(Scanner scanner) {
+        if (hasWinner()) {
+            board.print();
+            System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+            return askPlayAgain(scanner);
+        } else if (board.isFull()) {
+            board.print();
+            System.out.println("It's a draw!");
+            return askPlayAgain(scanner);
+        }
+        switchCurrentPlayer();
+        return true;
+    }
+
+    private boolean askPlayAgain(Scanner scanner) {
+        System.out.print("Play again? (y/n): ");
+        String answer = scanner.next();
+        if (answer.equals("y")) {
+            board.clear();
+            currentPlayer = player1;
+            return true;
+        }
+        return false;
     }
 
     private void switchCurrentPlayer() {
@@ -71,7 +75,6 @@ public class TicTacToe {
     private boolean hasWinner() {
         char[][] c = board.getCells();
         char m = currentPlayer.getMarker();
-
         for (int i = 0; i < 3; i++) {
             if (c[i][0] == m && c[i][1] == m && c[i][2] == m) {
                 return true;
@@ -80,14 +83,7 @@ public class TicTacToe {
                 return true;
             }
         }
-
-        if (c[0][0] == m && c[1][1] == m && c[2][2] == m) {
-            return true;
-        }
-        if (c[0][2] == m && c[1][1] == m && c[2][0] == m) {
-            return true;
-        }
-
-        return false;
+        return (c[0][0] == m && c[1][1] == m && c[2][2] == m)
+                || (c[0][2] == m && c[1][1] == m && c[2][0] == m);
     }
 }
